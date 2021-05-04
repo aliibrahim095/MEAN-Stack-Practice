618b34eb2fcf8dc4a52880e1b3842fb29985d7a3
aliibrahim095 <4liebrahim@gmail.com>
2021-05-04 18:30:35 +0200

    adding expansion

---
+++ src/app/post-list/post-list.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-post-list',
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.css']
+})
+export class PostListComponent implements OnInit {
+
+  posts=[
+    {title:"first post", content:"this is first post content"},
+    {title:"second post", content:"this is second post content"},
+    {title:"third post", content:"this is third post content"},
+    {title:"fourth post", content:"this is fourth post content"},
+  ]
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
